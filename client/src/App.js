80399537d1a04c1538658993cd3bd60ed4a95f02
Alexandr Fetisov <fetis_94@mail.ru>
2021-12-06 07:31:43 +0500

    add 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Apartment from "./pages/Apartment";
 import NavBar from "./components/NavBar/NavBar";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
                 <Routes>
                     <Route path='/apartments/:id' element={<Apartment/>}/>
                     <Route path='/' element={<MainPage/>}/>
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
         </div>
